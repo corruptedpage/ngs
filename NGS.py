7aa7e9ac8a72dc73d5a7cefd49452f1c4f41db33
corruptedpage <corruptedpage11@gmail.com>
2021-07-30 19:00:45 +0400

    Massive code overhaul + some changes
    Basically:
    • you have to close the game before saving/loading
    • code overhaul
    • added comments

--- NGS.py
+++ NGS.py
@@ -1,100 +1,118 @@
-import shutil, os, time, getpass
+from subprocess import call
+from os import system, mkdir, path
+from shutil import rmtree, copytree, copy
+from time import sleep
+from getpass import getuser
+from psutil import process_iter, NoSuchProcess, AccessDenied, ZombieProcess
+system('color 6')
 
-os.system('color 6')
+
+def process_running(processName):  # check if noita is running
+    for proc in process_iter():
+        try:
+            if processName.lower() in proc.name().lower():
+                return True
+        except (NoSuchProcess, AccessDenied, ZombieProcess):
+            pass
+    return False
+
+# idk why but yeah class for only one variable i am stupid as hell
 class var:
-    die = 'C:\\Users\\' + getpass.getuser() + '\\AppData\\LocalLow\\Nolla_Games_Noita\\'
+    save_folder = 'C:\\Users\\' + getuser() + '\\AppData\\LocalLow\\Nolla_Games_Noita\\'
 
-b_amount = 5
-if os.path.exists(var.die + 'load02'):
+
+save_folders_amount = 5
+if path.exists(var.save_folder + 'NGSload\\load02'):
     pass
 else:
-    for i in range(b_amount):
-        sovr = 'load0' + str(i+1)
-        os.mkdir(var.die + sovr)
+    mkdir(var.save_folder + 'NGSload')
+    for i in range(save_folders_amount):
+        save_folder = 'NGSload\\' + 'load0' + str(i + 1)
+        mkdir(var.save_folder + save_folder)
 
 
-def savenload (sorv = 's', notch = '0'):
-    if sorv == 's':
-        die1 = var.die + 'load0' + notch + '\\'
-        die = var.die + 'save00\\'
+def save(action='s', notch='0'):  # save and load func
+    if action == 's': # s = save, else = load
+        first_folder = var.save_folder + 'NGSload\\load0' + notch + '\\'
+        second_folder = var.save_folder + 'save00\\'
     else:
-        die1 = var.die + 'save00\\'
-        die = var.die + 'load0' + notch + '\\'
-    if os.path.exists(die1 + 'world\\'):
-        shutil.rmtree(die1)
-        time.sleep(0.1)
-        os.mkdir(die1)
-    time.sleep(0.1)
-    print('Copying persistent')
-    shutil.copytree(die + 'persistent\\', die1 + 'persistent\\')
+        first_folder = var.save_folder + 'save00\\'
+        second_folder = var.save_folder + 'NGSload\\load0' + notch + '\\'
+    print('Close noita.exe to continue.') # or else files get corrupted
+    while process_running('noita.exe'):
+        sleep(0.1)
+    if path.exists(first_folder + 'world\\'):  # delete the save folder if it is not empty
+        rmtree(first_folder)
+        sleep(0.1)
+        mkdir(first_folder)
+    sleep(0.1)
+    try:
+        print('Copying persistent') # copy the files to the second directory
+        copytree(second_folder + 'persistent\\', first_folder + 'persistent\\')
+        print('Copying stats')
+        copytree(second_folder + 'stats\\', first_folder + 'stats\\')
+        print('Copying world (might take a while)')
+        copytree(second_folder + 'world\\', first_folder + 'world\\')
+        print('Copying player.xml')
+        copy(second_folder + 'player.xml', first_folder + 'player.xml')
+        print('Copying world_state.xml')
+        copy(second_folder + 'world_state.xml', first_folder + 'world_state.xml')
+        print('Copying session_numbers.salakieli')
+        copy(second_folder + 'session_numbers.salakieli', first_folder + 'session_numbers.salakieli')
+        print('Copying mod_config.xml')
+        copy(second_folder + 'mod_config.xml', first_folder + 'mod_config.xml')
+        print('Operation completed successfully!\n')
+    except FileExistsError:  # yeah am i stupid? Of course i am! Idk why but i got an error when i didn't have this shit
+        rmtree(first_folder)
+        sleep(0.1)
+        mkdir(first_folder)
+        save(action, notch)
 
-    print('Copying stats')
-    shutil.copytree(die + 'stats\\', die1 + 'stats\\')
 
-    print('Copying world (might take a while)')
-    shutil.copytree(die + 'world\\', die1 + 'world\\')
-
-    print('Copying player.xml')
-    shutil.copy(die + 'player.xml', die1 + 'player.xml')
-
-    print('Copying world_state.xml')
-    shutil.copy(die + 'world_state.xml', die1 + 'world_state.xml')
-
-    print('Copying session_numbers.salakieli')
-    shutil.copy(die + 'session_numbers.salakieli', die1 + 'session_numbers.salakieli')
-
-    print('Copying mod_config.xml')
-    shutil.copy(die + 'mod_config.xml', die1 + 'mod_config.xml')
-
-    print('Operation completed successfully!')
-
-
-while 1 == 1:
-    co = int(input('   Menu:\n'
-                   '   1. Save\n'
-                   '   2. Load\n'
-                   '   3. delete save\n'
-                   '   4. instructions\n'
-                   '   IMPORTANT: TYPE A NUMBER OF THE OPTION YOU CHOSSE (1 FOR SAVING, ETC)\n'))
-    if co != 3:
-        if co == 1:
-            co = 's'
-        else:
-            co = 'l'
+while True:
+    co = input('   Menu:\n'
+               '     [1]  Save\n'
+               '     [2]  Load\n'
+               '     [3]  delete load folder\n'
+               '     [4]  Open noita.exe\n'
+               '   Type the number in [] to proceed\n>> ')
+    if co == '1' or co == '2':  # i was too stupid when i coded it in November, but i decided to leave it like
+        if co == '1':           # this cuz it fcking works lmao
+            co = 's'  # save
+        elif co == '2':
+            co = 'l'  # load
         print(f'   Choose save file:')
-        for i in range(b_amount):
-            sovr = 'load0' + str(i+1)
-            if os.path.exists(var.die + f'{sovr}\\world\\'):
+        for i in range(save_folders_amount):
+            save_folder = 'NGSload\\load0' + str(i + 1)
+            if path.exists(var.save_folder + f'{save_folder}\\world\\'):
                 if co == 'l':
-                    print(f'  #{i+1}')
+                    print(f'  #{i + 1}')
                 elif co == 's':
                     print(f'  #{i + 1} - FULL')
             else:
                 if co == 's':
-                    print(f'  #{i+1}')
-        co1 = input(f'[1-{b_amount}] >')
-        ch = input("   Are you sure? Press Enter if yes, type anything if you've changed your mind ")
-        if ch == '':
-            savenload(co, co1)
-    elif co == 3:
+                    print(f'  #{i + 1}')
+        co1 = input(f'[1-{save_folders_amount}] > ')
+        save(co, co1)
+    elif co == '3':  # delete save file
         print('Choose save file:')
-        for i in range(b_amount):
-            if os.path.exists(var.die + 'load0' + str(i+1) + '\\world\\'):
-                print(f'#{i+1}')
-            else:
-                pass
-        co1 = input(f'[1-{b_amount}] >')
-        ch = input('  Are you REALLY sure? Type "yes" to confirm ')
-        if ch == 'yes':
-            sovr = 'load0' + co1
-            print(sovr)
-            shutil.rmtree(var.die + sovr)
-            time.sleep(0.1)
-            os.mkdir(var.die + sovr)
+        for i in range(save_folders_amount):
+            if path.exists(var.save_folder + 'load0' + str(i + 1) + '\\world\\'):
+                print(f'#{i + 1}')
+        co1 = input(f'[1-{save_folders_amount}] > ')
+        ch = input('  Are you sure? Type anything to confirm ')
+        if ch != '':
+            save_folder = 'NGSload\\load0' + co1
+            print(save_folder)
+            rmtree(var.save_folder + save_folder)
+            sleep(0.1)
+            mkdir(var.save_folder + save_folder)
             print('   This save file is now gone forever...')
+    elif co == '4':
+        call(r"C:\Program Files (x86)\Steam\Steam.exe -applaunch 881100")  # nothing sus oficcer
 
     else:
-        print('   Everything is easy: first you need to choose whether youd like to save or load your save file\n'
-              '   next choose what file # you want.')
-    time.sleep(3)
-    os.system('cls')
+        print('   Did you missclick?\n'
+              '   Just choose what file # you want. type 1 and press enter')  # if you type 'dfgsdfhsadtjsdfgj'
+    sleep(3)
+    system('cls')
--- NGS_rus.py
+++ NGS_rus.py
@@ -1,100 +1,118 @@
-import shutil, os, time, getpass
+from subprocess import call
+from os import system, mkdir, path
+from shutil import rmtree, copytree, copy
+from time import sleep
+from getpass import getuser
+from psutil import process_iter, NoSuchProcess, AccessDenied, ZombieProcess
+system('color 6')
 
-os.system('color 6')
+
+def process_running(processName):  # Проверка, запущена ли нойта
+    for proc in process_iter():
+        try:
+            if processName.lower() in proc.name().lower():
+                return True
+        except (NoSuchProcess, AccessDenied, ZombieProcess):
+            pass
+    return False
+
+# Я хз почему, но класс с одной переменной, да, я ультратупой
 class var:
-    die = 'C:\\Users\\' + getpass.getuser() + '\\AppData\\LocalLow\\Nolla_Games_Noita\\'
+    save_folder = 'C:\\Users\\' + getuser() + '\\AppData\\LocalLow\\Nolla_Games_Noita\\'
 
-b_amount = 5
-if os.path.exists(var.die + 'load02'):
+
+save_folders_amount = 5
+if path.exists(var.save_folder + 'NGSload\\load02'):
     pass
 else:
-    for i in range(b_amount):
-        sovr = 'load0' + str(i+1)
-        os.mkdir(var.die + sovr)
+    mkdir(var.save_folder + 'NGSload')
+    for i in range(save_folders_amount):
+        save_folder = 'NGSload\\' + 'load0' + str(i + 1)
+        mkdir(var.save_folder + save_folder)
 
 
-def savenload (sorv = 's', notch = '0'):
-    if sorv == 's':
-        die1 = var.die + 'load0' + notch + '\\'
-        die = var.die + 'save00\\'
+def save(action='s', notch='0'):  # функция чтоб сохранить и загрузить файлы
+    if action == 's': # s = save, else = load
+        first_folder = var.save_folder + 'NGSload\\load0' + notch + '\\'
+        second_folder = var.save_folder + 'save00\\'
     else:
-        die1 = var.die + 'save00\\'
-        die = var.die + 'load0' + notch + '\\'
-    if os.path.exists(die1 + 'world\\'):
-        shutil.rmtree(die1)
-        time.sleep(0.1)
-        os.mkdir(die1)
-    time.sleep(0.1)
-    print('Копируем persistent')
-    shutil.copytree(die + 'persistent\\', die1 + 'persistent\\')
+        first_folder = var.save_folder + 'save00\\'
+        second_folder = var.save_folder + 'NGSload\\load0' + notch + '\\'
+    print('Закройте noita.exe для продолжения.') # или файлы могут повредиться
+    while process_running('noita.exe'):
+        sleep(0.1)
+    if path.exists(first_folder + 'world\\'):  # удалить папку, если она пуста
+        rmtree(first_folder)
+        sleep(0.1)
+        mkdir(first_folder)
+    sleep(0.1)
+    try:
+        print('Копируем persistent') # копирование файлов в другую папку
+        copytree(second_folder + 'persistent\\', first_folder + 'persistent\\')
+        print('Копируем stats')
+        copytree(second_folder + 'stats\\', first_folder + 'stats\\')
+        print('Копируем world (может занять много времени)')
+        copytree(second_folder + 'world\\', first_folder + 'world\\')
+        print('Копируем player.xml')
+        copy(second_folder + 'player.xml', first_folder + 'player.xml')
+        print('Копируем world_state.xml')
+        copy(second_folder + 'world_state.xml', first_folder + 'world_state.xml')
+        print('Копируем session_numbers.salakieli')
+        copy(second_folder + 'session_numbers.salakieli', first_folder + 'session_numbers.salakieli')
+        print('Копируем mod_config.xml')
+        copy(second_folder + 'mod_config.xml', first_folder + 'mod_config.xml')
+        print('Операция завершена успешно!\n')
+    except FileExistsError:  # Тупой ли я? Конечно же да! Я хз почему но без этой херни выдает ошибку
+        rmtree(first_folder)
+        sleep(0.1)
+        mkdir(first_folder)
+        save(action, notch)
 
-    print('Копируем stats')
-    shutil.copytree(die + 'stats\\', die1 + 'stats\\')
 
-    print('Копируем world (Может занять много времени)')
-    shutil.copytree(die + 'world\\', die1 + 'world\\')
-
-    print('Копируем player.xml')
-    shutil.copy(die + 'player.xml', die1 + 'player.xml')
-
-    print('Копируем world_state.xml')
-    shutil.copy(die + 'world_state.xml', die1 + 'world_state.xml')
-
-    print('Копируем session_numbers.salakieli')
-    shutil.copy(die + 'session_numbers.salakieli', die1 + 'session_numbers.salakieli')
-
-    print('Копируем mod_config.xml')
-    shutil.copy(die + 'mod_config.xml', die1 + 'mod_config.xml')
-
-    print('Операция завершена успешно!')
-
-
-while 1 == 1:
-    co = int(input('   Меню:\n'
-                   '   1. Сохранить\n'
-                   '   2. Загрузить\n'
-                   '   3. Удалить сохранение\n'
-                   '   4. Инструкция\n'
-                   ' ВАЖНО: ВЫБРАТЬ НОМЕР ОПЦИИ (1 ДЛЯ СОХРАНЕНИЯ, НАПРИМЕР)\n'))
-    if co != 3:
-        if co == 1:
-            co = 's'
-        else:
-            co = 'l'
-        print(f'   Выберите ячейку сохранения:')
-        for i in range(b_amount):
-            sovr = 'load0' + str(i+1)
-            if os.path.exists(var.die + f'{sovr}\\world\\'):
+while True:
+    co = input('   Меню:\n'
+               '     [1]  Сохранить\n'
+               '     [2]  Загрузить\n'
+               '     [3]  Удалить папку сохранения\n'
+               '     [4]  Открыть noita.exe\n'
+               '   Напишите цифру из [] чтобы продолжить\n>> ')
+    if co == '1' or co == '2':  # Я такую хрень написал в Ноябре, но оставлю это не тронутым
+        if co == '1':           # тк это бл работает
+            co = 's'  # сохранить
+        elif co == '2':
+            co = 'l'  # загрузить
+        print(f'   Выберите папку сохранения:')
+        for i in range(save_folders_amount):
+            save_folder = 'NGSload\\load0' + str(i + 1)
+            if path.exists(var.save_folder + f'{save_folder}\\world\\'):
                 if co == 'l':
-                    print(f'  #{i+1}')
+                    print(f'  #{i + 1}')
                 elif co == 's':
                     print(f'  #{i + 1} - FULL')
             else:
                 if co == 's':
-                    print(f'  #{i+1}')
-        co1 = input(f'[1-{b_amount}] >')
-        ch = input("   Вы уверены? Нажмите [Enter] ничего не вводя, если да. Введите что-то для отмены ")
-        if ch == '':
-            savenload(co, co1)
-    elif co == 3:
-        print('Выберите ячейку сохранения:')
-        for i in range(b_amount):
-            if os.path.exists(var.die + 'load0' + str(i+1) + '\\world\\'):
-                print(f'#{i+1}')
-            else:
-                pass
-        co1 = input(f'[1-{b_amount}] >')
-        ch = input('  Вы ТОЧНО уверены? Напишите "да" чтобы продолжить ')
-        if ch == 'да':
-            sovr = 'load0' + co1
-            print(sovr)
-            shutil.rmtree(var.die + sovr)
-            time.sleep(0.1)
-            os.mkdir(var.die + sovr)
-            print('   This save file is now gone forever...')
+                    print(f'  #{i + 1}')
+        co1 = input(f'[1-{save_folders_amount}] > ')
+        save(co, co1)
+    elif co == '3':  # Удалить файл сохранения
+        print('   Выберите папку сохранения:')
+        for i in range(save_folders_amount):
+            if path.exists(var.save_folder + 'load0' + str(i + 1) + '\\world\\'):
+                print(f'#{i + 1}')
+        co1 = input(f'[1-{save_folders_amount}] > ')
+        ch = input('  Вы уверены? Напишите что-то чтобы продолжить >')
+        if ch != '':
+            save_folder = 'NGSload\\load0' + co1
+            print(save_folder)
+            rmtree(var.save_folder + save_folder)
+            sleep(0.1)
+            mkdir(var.save_folder + save_folder)
+            print('   Сохранение удалено навсегда...')
+    elif co == '4':
+        call(r"C:\Program Files (x86)\Steam\Steam.exe -applaunch 881100")  # Ничего подозрительного
 
     else:
-        print('   Все просто: необходимо выбрать сохранить или загрузить игру, потом выбрать ячейку для выгрузки файлов сохранения.'
-              '   \n')
-    time.sleep(3)
-    os.system('cls')
+        print('   Вы мисскликнули?\n'
+              '   Напишите число в [].')  # если ты напишешь 'dfgsdfhsadtjsdfgj'
+    sleep(3)
+    system('cls')
